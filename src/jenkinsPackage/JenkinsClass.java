943b3bcf3579675cd6d0ec0e7d8d6c059f5b25f1
Chandan <chandan.vviet@live.com>
2016-08-13 18:34:10 +0530

    sdffs

--- src/jenkinsPackage/JenkinsClass.java
+++ src/jenkinsPackage/JenkinsClass.java
@@ -4,18 +4,46 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 public class JenkinsClass {
 	
+	ExtentReports e = new ExtentReports("./results/ExtentReports.html");
 	@Test
 	public void testA(){
 		
-		WebDriver driver= new FirefoxDriver();
+		 ExtentTest t=e.startTest("test 1");
+		 t.log(LogStatus.PASS, "hiiiiiiii");
+		 
+		 WebDriver driver= new FirefoxDriver();
+		 driver.get("https://www.google.com/");
+			System.out.println("running test A");
+			driver.quit();
+			
+		 e.endTest(t);
+		 
 		
-		driver.get("https://www.google.com/");
-		System.out.println("running test A");
-		driver.quit();
+		
+		
 		
 		
 	}
-
+	
+	@Test
+	public void testB()
+	{
+		
+		 ExtentTest t=e.startTest("test 1");
+		 t.log(LogStatus.PASS, "BYEEEEEEEEEEEEEE");
+		 
+		 WebDriver driver= new FirefoxDriver();
+		 driver.get("https://www.google.com/");
+			System.out.println("running test B");
+			driver.quit();
+			
+		 e.endTest(t);
+		 e.flush();
+	}
 }
